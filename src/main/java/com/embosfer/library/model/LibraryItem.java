9f365cc75cb0f5a6381478897a746b6ae7783126
embosfer <e.bosch.ferrando@gmail.com>
2016-11-12 16:47:21 +0000

    Add null checks and javadoc. Clean up equals method

--- src/main/java/com/embosfer/library/model/LibraryItem.java
+++ src/main/java/com/embosfer/library/model/LibraryItem.java
@@ -1,6 +1,10 @@
 package com.embosfer.library.model;
 
+import java.util.Objects;
+
 /**
+ * Represents the loanable items in the library
+ * 
  * @author embosfer
  *
  */
@@ -8,18 +12,34 @@ public class LibraryItem {
 
 	private static final String SEP = " ";
 
+	/**
+	 * Book, DVD, VHS
+	 */
 	public static enum LibraryItemType {
 		Book, DVD, VHS
 	}
 
 	private final long uniqueID;
-	private final long bookID;
+	private final long itemID;
 	private final LibraryItemType type;
 	private final String title;
 
-	public LibraryItem(long uniqueID, long bookID, LibraryItemType type, String title) {
+	/**
+	 * @param uniqueID
+	 *            The item's unique ID.
+	 * @param itemID
+	 *            The item ID. This item will be the same across multiple copies
+	 *            of the item in the library
+	 * @param type
+	 *            The item's {@link LibraryItemType}. Must not be null
+	 * @param title
+	 *            The item's title. Must not be null
+	 */
+	public LibraryItem(long uniqueID, long itemID, LibraryItemType type, String title) {
+		Objects.requireNonNull(type);
+		Objects.requireNonNull(title);
 		this.uniqueID = uniqueID;
-		this.bookID = bookID;
+		this.itemID = itemID;
 		this.type = type;
 		this.title = title;
 	}
@@ -32,10 +52,10 @@ public class LibraryItem {
 	}
 
 	/**
-	 * @return the bookID
+	 * @return the item ID
 	 */
-	public long getBookID() {
-		return bookID;
+	public long getItemID() {
+		return itemID;
 	}
 
 	/**
@@ -52,16 +72,13 @@ public class LibraryItem {
 		return title;
 	}
 
-	// FIXME add equals
-	// FIXME add hashcode
 	// FIXME add iterable and iterator
-	// FIXME add javadoc
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (bookID ^ (bookID >>> 32));
+		result = prime * result + (int) (itemID ^ (itemID >>> 32));
 		result = prime * result + ((title == null) ? 0 : title.hashCode());
 		result = prime * result + ((type == null) ? 0 : type.hashCode());
 		result = prime * result + (int) (uniqueID ^ (uniqueID >>> 32));
@@ -72,26 +89,23 @@ public class LibraryItem {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (getClass() != obj.getClass())
+		if (!(obj instanceof LibraryItem))
 			return false;
-		LibraryItem other = (LibraryItem) obj;
-		if (bookID != other.bookID)
+		LibraryItem that = (LibraryItem) obj;
+		if (uniqueID != that.uniqueID)
 			return false;
-		if (title == null) {
-			if (other.title != null)
-				return false;
-		} else if (!title.equals(other.title))
+		if (itemID != that.itemID)
 			return false;
-		if (type != other.type)
+		if (!this.title.equals(that.title))
 			return false;
-		if (uniqueID != other.uniqueID)
+		if (type != that.type)
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return new StringBuilder("UniqueID: ").append(uniqueID).append(SEP).append("BookID: ").append(bookID)
+		return new StringBuilder("UniqueID: ").append(uniqueID).append(SEP).append("ItemID: ").append(itemID)
 				.append(SEP).append("Type: ").append(type).append(SEP).append("Title: ").append(title).toString();
 	}
 
