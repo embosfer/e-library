785ceac3f9feb3a8bae7395d77b79dcae97499f4
embosfer <e.bosch.ferrando@gmail.com>
2016-11-12 08:42:44 +0000

    Implement LibraryController, add Loan and refactor checks

--- src/main/java/com/embosfer/library/LoanManager.java
+++ src/main/java/com/embosfer/library/LoanManager.java
@@ -7,11 +7,13 @@ import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 import com.embosfer.library.model.LibraryItem;
+import com.embosfer.library.model.Loan;
 import com.embosfer.library.model.User;
 
-public class LoanManager {
+public class LoanManager implements LibraryController {
 
 	// TODO: tune it to size
 	private final ConcurrentMap<Long, LibraryItem> loanedItems = new ConcurrentHashMap<>();
@@ -40,19 +42,36 @@ public class LoanManager {
 	}
 
 	// FIXME need the user dimension
-	public boolean loan(LibraryItem item, User user, int days) {
-		Objects.requireNonNull(item, "LibraryItem cannot be null");
-		Objects.requireNonNull(user, "User cannot be null");
+	@Override
+	public boolean borrow(LibraryItem item, User user, int days) {
+		check(item, user);
 		if (days < 1) throw new IllegalArgumentException("days must be > 0");
 		
 		LibraryItem itemToLoan = loanedItems.putIfAbsent(item.getUniqueID(), item);
 		if (itemToLoan == null) {
+			// borrowing successful
 			// TODO create transaction
 			LocalDateTime now = LocalDateTime.now();
-			LocalDateTime returningDate = now.plusDays(days);
+			new Loan(user, item.getUniqueID(), now, now.plusDays(days));
+			overdueItemsNotifier.schedule(() -> System.err.println("Item " + itemToLoan + " is overdue!"), days, TimeUnit.DAYS);
 			return true;
 		} else {
 			return false;
 		}
+	}
+	
+	@Override
+	public boolean returnItem(LibraryItem item, User user) {
+		check(item, user);
+		LibraryItem itemReturned = loanedItems.remove(item.getUniqueID());
+		if (itemReturned != null) {
+			
+		}
+		return false;
+	}
+	
+	private void check(LibraryItem item, User user) {
+		Objects.requireNonNull(item, "LibraryItem cannot be null");
+		Objects.requireNonNull(user, "User cannot be null");
 	}
 }
