5b04732d48ff833e63b9c4c087fb829648fd26b1
embosfer <e.bosch.ferrando@gmail.com>
2016-11-13 16:57:13 +0000

    Add hashcode

--- src/main/java/com/embosfer/library/model/LibraryItemCopy.java
+++ src/main/java/com/embosfer/library/model/LibraryItemCopy.java
@@ -37,6 +37,15 @@ public class LibraryItemCopy {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + item.hashCode();
+		result = prime * result + (int) (uniqueID ^ (uniqueID >>> 32));
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
